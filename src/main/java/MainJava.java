12857c3a95572fe16df8fce0b5e322a956f18932
khashab2 <khashab2@illinois.edu>
2015-08-24 19:24:59 -0500

    initial commit

---
+++ src/main/java/MainJava.java
@@ -0,0 +1,3 @@
+public static void main(String [ ] args) { 
+    println("HELLOW WORLD FROM JAVA! "); 
+}
---
+++ src/main/scala/MainScala.scala
@@ -0,0 +1,3 @@
+def main(args: Array[String]): Unit = { 
+	println("HELLO WORLD from SCALA! ")
+}
